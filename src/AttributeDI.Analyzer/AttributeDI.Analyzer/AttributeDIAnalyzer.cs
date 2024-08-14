80da164f748cf51e49753f99db609f1b35bc6e53
Mike Garvey <mike.garvey@yevrag35.com>
2024-08-14 18:57:36 -0400

    Starting analyzer work

--- src/AttributeDI.Analyzer/AttributeDI.Analyzer/AttributeDIAnalyzer.cs
+++ src/AttributeDI.Analyzer/AttributeDI.Analyzer/AttributeDIAnalyzer.cs
@@ -5,26 +5,30 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 
+#nullable enable
 namespace AttributeDI.Analyzer
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class AttributeDIAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "AttributeDI";
+        public const string DiagnosticId = "ADI-001";
+        private const string _attName = "ServiceRegistrationAttribute";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private static readonly LocalizableString StaticAbstractRuleTitle = new LocalizableResourceString(nameof(Resources.StaticAbstractRuleTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString StaticAbstractRuleMessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString StaticAbstractRuleDescription = new LocalizableResourceString(nameof(Resources.StaticAbstractRuleDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Usage";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static readonly DiagnosticDescriptor StaticAbstractRule = new(DiagnosticId, StaticAbstractRuleTitle, StaticAbstractRuleMessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: StaticAbstractRuleDescription);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(StaticAbstractRule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -40,25 +44,22 @@ namespace AttributeDI.Analyzer
         {
             // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-            if (namedTypeSymbol.TypeKind != TypeKind.Class && namedTypeSymbol.TypeKind != TypeKind.Struct)
+            if ((namedTypeSymbol.TypeKind != TypeKind.Class && namedTypeSymbol.TypeKind != TypeKind.Struct)
+                ||
+                !TryGetSRAttribute(namedTypeSymbol, out AttributeData? attribute))
             {
                 return;
             }
 
-            if (namedTypeSymbol.IsAbstract || namedTypeSymbol.IsStatic)
+            if (IsAbstractOrStatic(namedTypeSymbol, in context))
             {
-                
+                return;
             }
 
-            var attributes = namedTypeSymbol.GetAttributes();
-
-            if (attributes.Length > 0 && attributes.Any(x => x.AttributeClass.Name == "ServiceRegistrationAttribute"))
+            var constructors = namedTypeSymbol.Constructors;
+            if (constructors.Length <= 0 || !constructors.Any(x => x.DeclaredAccessibility == Accessibility.Public))
             {
-                var constructors = namedTypeSymbol.Constructors;
-                if (constructors.Length <= 0 || !constructors.Any(x => x.DeclaredAccessibility == Accessibility.Public))
-                {
-                    
-                }
+
             }
 
             //// Find just those named type symbols with names containing lowercase letters.
@@ -71,9 +72,9 @@ namespace AttributeDI.Analyzer
             //}
         }
 
-        private static bool HasServiceRegistrationAttribute(INamedTypeSymbol symbol)
+        private static bool EqualsSRAttribute(AttributeData attribute)
         {
-            return symbol.GetAttributes().Any(x => x.AttributeClass.Name == "ServiceRegistrationAttribute");
+            return _attName.Equals(attribute.AttributeClass?.Name, StringComparison.Ordinal);
         }
         private static bool HasProperConstructor(INamedTypeSymbol symbol)
         {
@@ -82,6 +83,41 @@ namespace AttributeDI.Analyzer
                                                        &&
                                                        !x.IsStatic);
             return true;
+        }
+        private static bool HasServiceRegistrationAttribute(INamedTypeSymbol symbol)
+        {
+            return symbol.GetAttributes().Any(EqualsSRAttribute);
+        }
+        private static bool IsAbstractOrStatic(INamedTypeSymbol symbol, in SymbolAnalysisContext context)
+        {
+            if (symbol.IsAbstract || symbol.IsStatic)
+            {
+                Diagnostic.Create(StaticAbstractRule, symbol.Locations[0], symbol.Name);
+                return true;
+            }
+
+            return false;
+        }
+        private static bool IsStructAndServiceTypeIsNotInterface(AttributeData data, INamedTypeSymbol symbol, in SymbolAnalysisContext context)
+        {
+            if (symbol.TypeKind != TypeKind.Struct)
+            {
+                return false;
+            }
+
+            var constructorArgs = data.ConstructorArguments;
+            var namedArgs = data.NamedArguments;
+            if (constructorArgs.Length == 0 && namedArgs.Length == 0)
+            {
+
+            }
+        }
+        private static bool TryGetSRAttribute(INamedTypeSymbol symbol, [NotNullWhen(true)] out AttributeData? attribute)
+        {
+            ImmutableArray<AttributeData> attributes = symbol.GetAttributes();
+            attribute = attributes.FirstOrDefault(EqualsSRAttribute);
+
+            return attribute is not null;
         }
     }
 }
